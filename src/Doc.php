53c761d16849c9232282fde789cbed8a20100b74
zhangweiwei0326 <1763692101@qq.com>
2017-10-27 20:07:32 +0800

    update

--- src/Doc.php
+++ src/Doc.php
@@ -122,8 +122,8 @@ class Doc
                 $method = $reflection->getMethod($action);
                 $doc = new DocParser();
                 $action_doc = $doc->parse($method->getDocComment());
-                $action_doc['header'] = isset($action_doc['header']) ? array_merge($class_doc['header'], $action_doc['header']) : [];
-                $action_doc['param'] = isset($action_doc['param']) ? array_merge($class_doc['param'], $action_doc['param']) : [];
+                $action_doc['header'] = isset($action_doc['header']) ? array_merge($class_doc['header'], $action_doc['header']) : $class_doc['header'];
+                $action_doc['param'] = isset($action_doc['param']) ? array_merge($class_doc['param'], $action_doc['param']) : $class_doc['param'];
             }
         }
         return $action_doc;
